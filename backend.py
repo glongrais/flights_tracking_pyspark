3c9a7e71a54304b2782a8afdcbf40cbbe0baa93b
Guillaume Longrais <guillaume.longrais@orange.fr>
2022-04-19 11:59:19 +0200

    Linked the backend

--- backend.py
+++ backend.py
@@ -4,8 +4,5 @@ from pyspark.sql import SparkSession
 sc = SparkContext("local", "Simple App")
 spark = SparkSession.builder.getOrCreate()
 
-airports_df = spark.read.csv("./Datasets/airports.csv", header=True)
-flights_small_df = spark.read.csv("./Datasets/flights_small.csv", header=True)
-planes_df = spark.read.csv("./Datasets/planes.csv", header=True)
-
-planes_df.show()
+def loadFile(path):
+    return spark.read.csv(path, header=True)
--- main.py
+++ main.py
@@ -3,6 +3,11 @@ import backend
 
 def main():
     root = GUI.init("Flights Tracker")
+
+    airports_df = backend.loadFile("./Datasets/airports.csv")
+    flights_small_df = backend.loadFile("./Datasets/flights_small.csv")
+    planes_df = backend.loadFile("./Datasets/planes.csv")
+
     root.mainloop()
 
 if __name__ == "__main__":
